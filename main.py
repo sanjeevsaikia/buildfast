ed240676cbab58cbc3dac27f756a8e07bb896eb0
sanjeevsaikia <sanjeev.saikia@gmail.com>
2025-09-28 01:32:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ tweet_chain = tweet_prompt | gemini_model
 
 import streamlit as st
 
-st.header("🐦 Tweet Generator")
+st.header("🐦 Sanjeev's Tweet Generator")
 
 st.subheader("Generate tweets using Generative AI 🤖")
 
